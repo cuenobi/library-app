4c93df1fbc7e1aeae226c5c5a333b17dfa5a4e68
cuenobi <sitthikon.work@gmail.com>
2025-02-14 13:57:45 +0700

    Refactor user adapter to integrate model package and add user creation and retrieval methods

--- backend/internal/adapter/postgres/user.go
+++ backend/internal/adapter/postgres/user.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "gorm.io/gorm"
+import (
+	"library-service/internal/model"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	conn *gorm.DB
@@ -14,4 +18,12 @@ func NewUser(conn *gorm.DB) *User {
 
 func (u *User) HasUsername(username string) (bool, error) {
 	return false, nil
+}
+
+func (u *User) CreateUser(user *model.User) error {
+	return nil
+}
+
+func (u *User) GetUserByUsername(username string) (*model.User, error) {
+	return &model.User{}, nil
 }
