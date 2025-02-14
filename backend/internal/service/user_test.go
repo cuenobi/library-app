22acabb31353c83d79f4c6e804ce5022b37bc2d1
cuenobi <sitthikon.work@gmail.com>
2025-02-14 10:55:56 +0700

    Add unit tests for user service, including user creation and authentication scenarios

--- backend/internal/service/user_test.go
+++ backend/internal/service/user_test.go
@@ -1,13 +1,16 @@
 package service
 
 import (
+	"fmt"
 	"testing"
 
 	"library-service/internal/constant"
 	"library-service/internal/domain/model"
 	"library-service/mocks"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestUserServiceSuite(t *testing.T) {
@@ -21,23 +24,31 @@ type UserServiceSuite struct {
 	mockJwt       mocks.JWT
 	mockUser      *model.User
 	mockLibrarian *model.User
+	mockPassword  string
 }
 
 func (u *UserServiceSuite) SetupTest() {
 	u.mockUserRepo = *mocks.NewUserRepository(u.T())
 	u.mockJwt = *mocks.NewJWT(u.T())
+
+	u.mockPassword = "password"
+	hashPwd, _ := bcrypt.GenerateFromPassword([]byte(u.mockPassword), bcrypt.DefaultCost)
+
 	u.mockUser = &model.User{
 		Username: "foo",
-		Password: "password",
+		Password: string(hashPwd),
 		Name:     "bar",
 		Role:     constant.MemberRole,
 	}
+
 	u.mockLibrarian = &model.User{
 		Username: "bar",
-		Password: "password",
+		Password: string(hashPwd),
 		Name:     "foo",
 		Role:     constant.Librarian,
 	}
+
+	u.service = NewUserService(&u.mockUserRepo, &u.mockJwt)
 }
 
 func (u *UserServiceSuite) TearDownTest() {
@@ -48,4 +59,105 @@ func (u *UserServiceSuite) TearDownTest() {
 func (u *UserServiceSuite) SetupSubTest() {
 	u.TearDownTest()
 	u.SetupTest()
+}
+
+func (u *UserServiceSuite) TestCreateUserSuccess() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, nil)
+	u.mockUserRepo.EXPECT().CreateUser(mock.Anything).Return(nil)
+
+	err := u.service.CreateUser(u.mockUser)
+	u.Require().NoError(err)
+}
+
+func (u *UserServiceSuite) TestCreateUserInvalidRole() {
+	err := u.service.CreateUser(u.mockLibrarian)
+	u.Require().Error(err)
+	u.Require().Equal("invalid role", err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateUserUsernameAlreadyExist() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(true, nil)
+
+	err := u.service.CreateUser(u.mockUser)
+	u.Require().Error(err)
+	u.Require().Equal("username already exist", err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateUserHasUsernameError() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, fmt.Errorf(mock.Anything))
+
+	err := u.service.CreateUser(u.mockUser)
+	u.Require().Error(err)
+	u.Require().Equal(mock.Anything, err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateUserCreateUserError() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, nil)
+	u.mockUserRepo.EXPECT().CreateUser(mock.Anything).Return(fmt.Errorf(mock.Anything))
+
+	err := u.service.CreateUser(u.mockUser)
+	u.Require().Error(err)
+	u.Require().Equal(mock.Anything, err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateLibrarianSuccess() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, nil)
+	u.mockUserRepo.EXPECT().CreateUser(mock.Anything).Return(nil)
+
+	err := u.service.CreateLibrarian(u.mockLibrarian)
+	u.Require().NoError(err)
+}
+
+func (u *UserServiceSuite) TestCreateLibrarianInvalidRole() {
+	err := u.service.CreateLibrarian(u.mockUser)
+	u.Require().Error(err)
+	u.Require().Equal("invalid role", err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateLibrarianUsernameAlreadyExist() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(true, nil)
+
+	err := u.service.CreateLibrarian(u.mockLibrarian)
+	u.Require().Error(err)
+	u.Require().Equal("username already exist", err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateLibrarianHasUsernameError() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, fmt.Errorf(mock.Anything))
+
+	err := u.service.CreateLibrarian(u.mockLibrarian)
+	u.Require().Error(err)
+	u.Require().Equal(mock.Anything, err.Error())
+}
+
+func (u *UserServiceSuite) TestCreateLibrarianCreateUserError() {
+	u.mockUserRepo.EXPECT().HasUsername(mock.Anything).Return(false, nil)
+	u.mockUserRepo.EXPECT().CreateUser(mock.Anything).Return(fmt.Errorf(mock.Anything))
+
+	err := u.service.CreateLibrarian(u.mockLibrarian)
+	u.Require().Error(err)
+	u.Require().Equal(mock.Anything, err.Error())
+}
+
+func (u *UserServiceSuite) TestAuthenticationSuccess() {
+	u.mockUserRepo.EXPECT().GetUserByUsername(mock.Anything).Return(u.mockUser, nil)
+	u.mockJwt.EXPECT().Generate(mock.Anything, mock.Anything).Return(mock.Anything)
+
+	_, err := u.service.Authentication(u.mockUser.Username, u.mockPassword)
+	u.Require().NoError(err)
+}
+
+func (u *UserServiceSuite) TestAuthenticationGetUserError() {
+	u.mockUserRepo.EXPECT().GetUserByUsername(mock.Anything).Return(nil, fmt.Errorf(mock.Anything))
+
+	_, err := u.service.Authentication(u.mockUser.Username, u.mockPassword)
+	u.Require().Error(err)
+	u.Require().Equal(mock.Anything, err.Error())
+}
+
+func (u *UserServiceSuite) TestAuthenticationCompareError() {
+	u.mockUserRepo.EXPECT().GetUserByUsername(mock.Anything).Return(u.mockUser, nil)
+
+	_, err := u.service.Authentication(u.mockUser.Username, u.mockUser.Password)
+	u.Require().Error(err)
 }
